60363e8ae5a2d095396680d1f7d21c7bc01e0147
Danylo Santoro <danylo@generous.studio>
2022-04-11 12:16:43 +0100

    fix font size at mobile

--- components/HighLight/styled.js
+++ components/HighLight/styled.js
@@ -14,12 +14,12 @@ export const Season = styled.h6`
 `;
 
 export const Title = styled.h1`
-  font-size: clamp(1rem, -0.875rem + 8.333vw, var(--font-size-5));
+  font-size: clamp(2rem, -0.875rem + 8.333vw, var(--font-size-5));
   font-weight: bold;
 `;
 
 export const Plot = styled.h2`
-  font-size: clamp(0.4rem, -0.875rem + 5.333vw, var(--font-size-4));
+  font-size: clamp(1rem, -0.875rem + 5.333vw, var(--font-size-4));
   font-weight: normal;
   max-width: 60%;
 `;
